807075466903213d4c7b7ff311e5a4d733150be1
Bruno Laino de Andrade <brunolaino_andrade99@hotmail.com>
2020-04-25 14:26:58 -0300

    Add JavaScript Standard Style and gitignore

--- content.js
+++ content.js
@@ -1,52 +1,48 @@
 const interval = setInterval(() => {
-    const header = document.querySelector("._3auIg");
+  const header = document.querySelector('._3auIg')
 
-    if(header){
-        
-        console.log(header);
-        clearInterval(interval);
-        
-        const button = document.createElement("button");
-        button.innerHTML = "2x";
-        button.classList.add("twoTimesButtonOff");
-        header.appendChild(button);
+  if (header) {
+    console.log(header)
+    clearInterval(interval)
 
+    const button = document.createElement('button')
+    button.innerHTML = '2x'
+    button.classList.add('twoTimesButtonOff')
+    header.appendChild(button)
 
-        document.addEventListener("mousemove", () =>{
-            if(button.classList.contains("twoTimesButtonOn")){
-                twoTimesSpeed();
-            }else{
-                oneTimeSpeed();
-            }
-        });
+    document.addEventListener('mousemove', () => {
+      if (button.classList.contains('twoTimesButtonOn')) {
+        twoTimesSpeed()
+      } else {
+        oneTimeSpeed()
+      }
+    })
 
-        button.addEventListener("click", () =>{
-            button.classList.toggle("twoTimesButtonOn"); //troca a classe(fixa) para essa que passei e vice versa
-            if(button.classList.contains("twoTimesButtonOff")) {
-                twoTimesSpeed();
-            }else{
-                oneTimeSpeed();
-            }
-        });
-        
-        function twoTimesSpeed(){
-            audios = document.querySelectorAll("audio");
-            console.log(audios);
-            audios.forEach((audio) => {
-                console.log(audio);
-                audio.playbackRate = 2;
-            });
-        }
+    button.addEventListener('click', () => {
+      button.classList.toggle('twoTimesButtonOn') // troca a classe(fixa) para essa que passei e vice versa
+      if (button.classList.contains('twoTimesButtonOff')) {
+        twoTimesSpeed()
+      } else {
+        oneTimeSpeed()
+      }
+    })
 
-        function oneTimeSpeed(){
-            audios = document.querySelectorAll("audio");
-            console.log(audios);
-            audios.forEach((audio) => {
-                console.log(audio);
-                audio.playbackRate = 1;
-            });
-        }
-
+    function twoTimesSpeed () {
+      audios = document.querySelectorAll('audio')
+      console.log(audios)
+      audios.forEach((audio) => {
+        console.log(audio)
+        audio.playbackRate = 2
+      })
     }
-},1000);
 
+    function oneTimeSpeed () {
+      audios = document.querySelectorAll('audio')
+      console.log(audios)
+      audios.forEach((audio) => {
+        console.log(audio)
+        audio.playbackRate = 1
+      })
+    }
+  }
+}, 1000)
